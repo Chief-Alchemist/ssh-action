ae9326e376c9406871ae7f5cba43616f2abc94b7
Game <96580990+Chief-Alchemist@users.noreply.github.com>
2023-02-08 14:23:37 +0000

    Update pass

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,9 +34,11 @@ cat $HOME/shell.sh
 
 echo Start Run Cmd
 
-if [ "$(echo -e "$INPUT_PASS")" = "" ]
+pass='$INPUT_PASS'
+
+if [ "$pass" = "" ]
 then
   sh -c "ssh "$INPUT_ARGS" -i "$KEYFILE" -o StrictHostKeyChecking=no -p "$INPUT_PORT" "${INPUT_USER}"@"${INPUT_HOST}" < "$HOME"/shell.sh"
 else
-  sh -c "sshpass -p "$(echo -e "$INPUT_PASS")" ssh "$INPUT_ARGS" -o StrictHostKeyChecking=no -p "$INPUT_PORT" "${INPUT_USER}"@"${INPUT_HOST}" < "$HOME"/shell.sh"
+  sh -c "sshpass -p "$pass" ssh "$INPUT_ARGS" -o StrictHostKeyChecking=no -p "$INPUT_PORT" "${INPUT_USER}"@"${INPUT_HOST}" < "$HOME"/shell.sh"
 fi
